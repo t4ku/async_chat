e6b4083b56f4d5ea14bae8466d82447c03c909d0
t4ku <t4ku@devkit.dreamhosters.com>
2010-06-06 16:07:51 +0900

    chat用のコントローラーを追加
    script/generate rspec_controller chat

---
+++ tascal/spec/controllers/chat_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ChatController do
+
+  #Delete this example and add some real ones
+  it "should use ChatController" do
+    controller.should be_an_instance_of(ChatController)
+  end
+
+end
