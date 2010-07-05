e413d7a043a2a1b3721db94a214138778328b1de
大川 琢 <ookawataku@ukkaga-aya-no-Mac-mini.local>
2010-07-06 05:15:53 +0900

    JSを変更

--- tascal/public/javascripts/chat.js
+++ tascal/public/javascripts/chat.js
@@ -2,13 +2,11 @@ $(document).ready(function() {
 	var current_time = (new Date).getTime();
 	var CONFIG = { debug: false
 				 , token:api_key
-	             , id: null    // set in onConnect
+	             , id: null    
 	             , last_message_time: current_time
-	             , focus: true //event listeners bound in onConnect
-	             , unread: 0 //updated in the message-processing loop
+	             , focus: true 
+	             , unread: 0
 	             };
-
-	var users = [];
 
 	// ユーティリティ
 	util = {
@@ -51,10 +49,8 @@ $(document).ready(function() {
 	    return;
 
 	  if (time == null) {
-	    // if the time is null or undefined, use the current time.
 	    time = new Date();
 	  } else if ((time instanceof Date) === false) {
-	    // if it's a timestamp, interpret it
 	    time = new Date(time);
 	  }
 
@@ -68,12 +64,12 @@ $(document).ready(function() {
 	  // テキスト
 	  text = util.toStaticHTML(text);
 
-	  // If the current user said this, add a special css class
+	  // 自分発言用
 	  var nick_re = new RegExp(CONFIG.nick);
 	  if (nick_re.exec(text))
 	    messageElement.addClass("personal");
 
-	  // replace URLs with links
+	  // メッセージ内のURLをリンクか
 	  text = text.replace(util.urlRE, '<a target="_blank" href="$&">$&</a>');
 
 	  var content = '  <span class="date">' + util.timeString(time) + '</span>'
@@ -83,38 +79,28 @@ $(document).ready(function() {
 	              ;
 	  messageElement.html(content);
 
-	  //the log is the stream that we view
 	  //$("#messages").append(messageElement);
 	  $("#messages").prepend(messageElement);
-
-	  //always view the most recent message when it is added
 	}
 
 	var transmission_errors = 0;
-	var first_poll = true;
 
 
-	//process updates if we have any, request updates from the server,
-	// and call again with response. the last part is like recursion except the call
-	// is being made from the response handler, and not at some point during the
-	// function's execution.
+	// ロングポール用再帰関数
 	function longPoll (data) {
 	  if (transmission_errors > 2) {
 	    showConnect();
 	    return;
 	  }
 
-	  //process any updates we may have
-	  //data will be null on the first call of longPoll
+
 	  if (data && data.messages) {
 	    for (var i = 0; i < data.messages.length; i++) {
 	      var message = data.messages[i];
 
-	      //track oldest message so we only request newer messages from server
 	      if (message.timestamp > CONFIG.last_message_time)
 	        CONFIG.last_message_time = message.timestamp;
 
-	      //dispatch new messages to their appropriate handlers
 	      switch (message.type) {
 	        case "msg":
 	          if(!CONFIG.focus){
@@ -124,14 +110,8 @@ $(document).ready(function() {
 	          break;
 	      }
 	    }
-
-	    //only after the first request for messages do we want to show who is here
-	    if (first_poll) {
-	      first_poll = false;
-	    }
 	  }
 
-	  //make another request
 	  $.ajax({ cache: false
 	         , type: "GET"
 	         , url:  "/stream/messages.json"
@@ -141,26 +121,18 @@ $(document).ready(function() {
 	         , error: function () {
 	             addMessage("", "long poll error. trying again...", new Date(), "error");
 	             transmission_errors += 1;
-	             //don't flood the servers on error, wait 10 seconds before retrying
 	             setTimeout(longPoll, 10*1000);
 	           }
 	         , success: function (data) {
 	             transmission_errors = 0;
-	             //if everything went well, begin another request immediately
-	             //the server will take a long time to respond
-	             //how long? well, it will wait until there is another message
-	             //and then it will return it to us and close the connection.
-	             //since the connection is closed when we get data, we longPoll again
 	             longPoll(data);
 	           }
 	         });
 	}
 
-	//submit a new message to the server
+	// メッセージ送信用関数
 	function send(msg) {
 	  if (CONFIG.debug === false) {
-	    // XXX should be POST
-	    // XXX should add to messages immediately
 	    jQuery.post("/stream/message.json",
 	 				{api_key:CONFIG.token,id: CONFIG.id, text: msg}, 
 					function (data) { 
@@ -176,30 +148,17 @@ $(document).ready(function() {
 
 	//Transition the page to the state that prompts the user for a nickname
 	function showConnect () {
-	  $("#connect").show();
-	  $("#loading").hide();
-	  $("#toolbar").hide();
-	  $("#nickInput").focus();
 	}
 
 	//transition the page to the loading screen
 	function showLoad () {
-	  $("#connect").hide();
-	  $("#loading").show();
-	  $("#toolbar").hide();
 	}
 
 	//transition the page to the main chat view, putting the cursor in the textfield
 	function showChat (nick) {
-	  $("#toolbar").show();
-	  $("#entry").focus();
-
-	  $("#connect").hide();
-	  $("#loading").hide();
-
 	}
 
-	//handle the server's response to our nickname and join request
+	// コールバック
 	function onConnect (session) {
 	  if (session.error) {
 	    alert("error connecting: " + session.error);
@@ -210,10 +169,8 @@ $(document).ready(function() {
 	  CONFIG.nick = session.nick;
 	  CONFIG.id   = session.id;
 
-	  //update the UI to show the chat
 	  showChat(CONFIG.nick);
 
-	  //listen for browser events so we know to update the document title
 	  $(window).bind("blur", function() {
 	    CONFIG.focus = false;
 	  });
@@ -224,59 +181,18 @@ $(document).ready(function() {
 	  });
 	}
 
-	//add a list of present chat members to the stream
-	function showUsers () {
-	  var users_string = users.length > 0 ? users.join(", ") : "(none)";
-	  addMessage("users:", users_string, new Date(), "notice");
-	  return false;
-	}
 
-  //submit new messages when the user hits enter if the message isnt blank
+  // 関数定義終わり、メイン処理
+
+  // チャットテキスト内でのEnterキーr補足
   $("#entry").keypress(function (e) {
     if (e.keyCode != 13 /* Return */) return;
     var msg = $("#entry").attr("value").replace("\n", "");
     if (!util.isBlank(msg)) send(msg);
-    $("#entry").attr("value", ""); // clear the entry field.
+    $("#entry").attr("value", "");
   });
 
-  $("#usersLink").click(showUsers);
-
-  //try joining the chat when the user clicks the connect button
-  $("#connectButton").click(function () {
-    //lock the UI while waiting for a response
-    showLoad();
-    var nick = $("#nickInput").attr("value");
-
-    //dont bother the backend if we fail easy validations
-    if (nick.length > 50) {
-      alert("Nick too long. 50 character max.");
-      showConnect();
-      return false;
-    }
-
-    //more validations
-    if (/[^\w_\-^!]/.exec(nick)) {
-      alert("Bad character in nick. Can only have letters, numbers, and '_', '-', '^', '!'");
-      showConnect();
-      return false;
-    }
-
-    //make the actual join request to the server
-    $.ajax({ cache: false
-           , type: "GET" // XXX should be POST
-           , dataType: "json"
-           , url: "/join"
-           , data: { nick: nick }
-           , error: function () {
-               alert("error connecting to server");
-               showConnect();
-             }
-           , success: onConnect
-           });
-    return false;
-  });
-
-  // update the clock every second
+  // 時間
   setInterval(function () {
     var now = new Date();
     $("#currentTime").text(util.timeString(now));
@@ -291,10 +207,6 @@ $(document).ready(function() {
   // remove fixtures
   $("#log table").remove();
 
-  //begin listening for updates right away
-  //interestingly, we don't need to join a room to get its updates
-  //we just don't show the chat stream to the user until we create a session
   longPoll();
-
   showConnect();
 });
